f68d83c5843b8e5ab39d88eba51cda6e22cb09e4
George Corser <gpcorser@svsu.edu>
2018-09-19 13:25:07 -0400

    updated on github.com

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ and open the template in the editor.
     <body>
         <?php
         // put your code here
-        echo "this is project 12";
+        echo "this is project 13";
         ?>
     </body>
 </html>
