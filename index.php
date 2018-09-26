a4e30c41112aa02180d5f30b48dbf84b2f751555
tdwheele <32941943+tdwheele@users.noreply.github.com>
2018-09-26 18:07:49 -0400

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ and open the template in the editor.
     <body>
         <?php
         // put your code here
-        echo "this is project 13";
+        echo "Prog02";
         ?>
     </body>
 </html>
